7177319470643d9c23d43376a6492095282318f4
Gabriel Molina <gabriel.mw3.gm@gmail.com>
2021-09-03 17:41:32 -0300

    Update Objeto.php

--- Objeto.php
+++ Objeto.php
@@ -31,13 +31,12 @@ class Objeto{
 
 
     public function __construct($ArrayObjeto){
-        $this->_objetoId= $ArrayObjeto["objetoId"];
+        $this->_objetoId= $ArrayObjeto["id"];
         $this->_tipoObjeto=$ArrayObjeto["tipoObjeto"];
         $this->_dataEncontrado=$ArrayObjeto["dataEncontrado"];
         $this->_localEntId=$ArrayObjeto["localEntId"];
         $this->_userEncontrouid=$ArrayObjeto["userEncontrouId"];
     }
-
     public function cadastraObjeto($inputTipoObjeto,$inputUserEncontrouId,$inputLocalEntId){
         $conn= connectionFactory();
         $stmt=$conn->prepare("INSERT INTO objetoencontrado(tipoObjeto,userEncontrouId,localEntId) values (:tipoObjeto,:userEncontrouId,:localEntId)");
@@ -47,5 +46,25 @@ class Objeto{
             "localEntId"=>$inputLocalEntId
         ]);
     }
+    public static function pegaObjeto($inputObjetoId){
+        $conn=connectionFactory();
+        $stmt=$conn->prepare("SELECT * FROM objetoEncontrado where id=:objetoId");
+        $stmt->execute([ 
+            "objetoId"=>$inputObjetoId
+        ]);
+        return new Objeto($stmt->fetch(PDO::FETCH_ASSOC));
+    }
+    public function alteraObjeto($inputId,$inputTipoObjeto,$inputLocalEntId,$inputUserEncontrouId){
+        $conn=connectionFactory();
+        $stmt=$conn->prepare("UPDATE objetoEncontrado set tipoObjeto=:tipoObjeto,localEntId=:localEntId,userEncontrouId=:userEncontrouId where id=:id");
+        $stmt->execute([
+            "id"=>$inputId,
+            "tipoObjeto"=>$inputTipoObjeto,
+            "localEntId"=>$inputLocalEntId,
+            "userEntId"=>$inputUserEncontrouId
+        ]);
+    }
+    
+
 }
 ?>
