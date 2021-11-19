f3302f6ac96a121c53f8ff0a476031e84cc74ae7
Gabriel Molina <gabriel.mw3.gm@gmail.com>
2021-11-18 21:28:24 -0300

    Update conn.php

--- conn.php
+++ conn.php
@@ -2,7 +2,7 @@
 
 function connectionFactory(){
     try{
-        $conn = new PDO('mysql:host=localhost;dbname=test', "gabriel", "Vendas@12");
+        $conn = new PDO('mysql:host=localhost;dbname=u626106768_EncontreAquiDb', "u626106768_encontreaquidb", "Eadb@2021");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
     }
