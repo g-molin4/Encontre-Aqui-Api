d5ced3d2e5359ff314d0fe763783f49db93e50ea
Gabriel Molina <gabriel@fintex.com.br>
2021-12-07 12:36:04 -0300

    Update objetos_cad.php

--- objetos_cad.php
+++ objetos_cad.php
@@ -3,22 +3,28 @@ include_once "Classes/Objeto.php";
 include_once "Classes/TiposObjeto.php";
 include_once "Classes/Orgao.php";
 session_start();
-// $usuario=json_decode($_SESSION["usuario"]);
-$tipo=$_GET["a"]??"";
-$id=$_GET["b"]??"";
-
-$objetos= json_decode(Objeto::objetosFeed($tipo,$id));
-
-for($i=0;$i<count($objetos);$i++){
-    $objetos[$i]->orgao=json_decode(json_encode(Orgao::pegaOrgao($objetos[$i]->orgaoId)));
-    $objetos[$i]->tipoObjeto=json_decode(json_encode(TiposObjeto::pegaTipoObjeto($objetos[$i]->tipoObjetoId)));
-    $objetos[$i]->imagem=json_decode(json_encode(Objeto::pegaImagens($objetos[$i]->id)));
-    // echo $objetos[$i]->orgao;
+if($_SESSION){
+    $usuario=json_decode($_SESSION["usuario"]);
+    $tipo=$_GET["a"]??"";
+    $id=$_GET["b"]??"";
+    if($usuario->nivel==2 && empty($tipo)){
+        die('{"erro":"Você não tem acesso a esse conteúdo"}');
+    }
+    else if ($usuario->nivel==1 && !empty($tipo)){
+        die('{"erro":"Você não tem acesso a esse conteúdo"}');
+    }
+    $objetos= json_decode(Objeto::objetosFeed($tipo,$id));
+    for($i=0;$i<count($objetos);$i++){
+        $objetos[$i]->orgao=json_decode(json_encode(Orgao::pegaOrgao($objetos[$i]->orgaoId)));
+        $objetos[$i]->tipoObjeto=json_decode(json_encode(TiposObjeto::pegaTipoObjeto($objetos[$i]->tipoObjetoId)));
+        $objetos[$i]->imagem=json_decode(json_encode(Objeto::pegaImagens($objetos[$i]->id)));
+        // echo $objetos[$i]->orgao;
+    }
+    // $objetos[0]->orgaoId;
+    echo json_encode($objetos);
+}
+else{
+    echo '{"erro":"Você não tem acesso a esse conteúdo"}';
 }
 
-// $objetos[0]->orgaoId;
-echo json_encode($objetos);
-
-
-// die(var_dump($objetos));
 ?>
