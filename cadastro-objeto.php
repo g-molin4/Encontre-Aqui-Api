1a129068ba1c626a614d42f33916fd8cfa20b9c6
Gabriel Molina <gabriel@fintex.com.br>
2021-12-07 12:02:40 -0300

    Update cadastro-objeto.php

--- cadastro-objeto.php
+++ cadastro-objeto.php
@@ -18,6 +18,9 @@
 
 	<body>
 		<?php
+        ini_set('display_errors',1);
+        ini_set('display_startup_erros',1);
+        error_reporting(E_ALL);
         $nivelMinimo=2;
         include "Classes/Objeto.php";
         include "Classes/TiposObjeto.php";
