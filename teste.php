b5f23d5868d00d0f9720265d1484ad552300ca74
Gabriel Molina <gabriel.mw3.gm@gmail.com>
2021-12-13 22:24:12 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -1,7 +1,35 @@
 <?php
-if(filter_var("silva2@fintex.com.br",FILTER_VALIDATE_EMAIL) !==false){
-    echo "tomanocu";
-}
-else
-    echo "batatinahfrita123";
+$to = "gabriel.mw3.gm@gmail.com";
+$subject = "faleconosco@encontreaqui.tech";
+
+$message = "
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Doe</td>
+</tr>
+</table>
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <Fale Conosco | Encontre Aqui>' . "\r\n";
+$headers .= 'Cc: myboss@example.com' . "\r\n";
+
+mail($to,$subject,$message,$headers);
 ?>
